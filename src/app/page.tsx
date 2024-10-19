72be095c10b9e72be95c7dc8cf1f2f270b5b21be
Kartik <kartik_a@mt.iitr.ac.in>
2024-10-19 23:28:05 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Card, CardContent, CardHeader, CardTitle } from "../components/ui/card";
-import { PatientAssessment } from '../components/patient-assessment';
 import { getPatients } from '@/actions/db';
 import { ClientPatientDashboard } from '@/components/patDash';
 
